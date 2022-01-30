f3c6c3c74cdae910b7762081a9704c99401f61d7
Caroline Monks <g00387655@gmit.ie>
2022-01-30 22:25:02 +0000

    week 02 BMI activity

---
+++ week02/bmi.py
@@ -0,0 +1,9 @@
+# bmi.py 
+# This program will calculate ones BMI
+# author: Caroline Monks
+
+h=float(input("Enter your height in meters: "))
+w=float(input("Enter your Weight in KG: "))
+
+BMI=w/(h*h)
+print( "BMI Calculated is: ",BMI)
